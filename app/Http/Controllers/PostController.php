e8a72bee1a1703e750b370b9751604446012c9aa
s-als <32486253+s-als@users.noreply.github.com>
2021-03-03 18:35:53 +0100

    Add single page post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,6 +14,12 @@ class PostController extends Controller
       return view('posts.index', ['posts' => $posts]); 
    }
 
+   public function show(Post $post){
+      return view('posts.show', [
+         'post' => $post
+      ]);
+   }
+
    public function store(Request $request){
       $this->validate($request, ['body' => 'required']);
 
