7869e51289a54620f4e0e7cdc946d30ec329598d
s-als <32486253+s-als@users.noreply.github.com>
2021-04-30 17:37:31 +0200

    Add search bar in page product

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,1 +1,27 @@
-console.log('test : customElements.js');
+console.log('test : custom.js');
+
+
+// Search bar:
+window.search = function () {
+    console.log('test : search');
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById('myUL');
+    li = ul.getElementsByTagName('li');
+    console.log(li);
+    console.log(li[1].getElementsByTagName("a")[0]);
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+        } else {
+          li[i].style.display = "none";
+        }
+    }
+}
+
+
+//Filter on search:
