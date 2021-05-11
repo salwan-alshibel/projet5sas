6419dda872fcd0ad02433b6c576d7f5e58f10067
s-als <32486253+s-als@users.noreply.github.com>
2021-05-11 15:28:17 +0200

    Add homeDashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,13 +2,14 @@
 
 
 @section('content')
-    <div class="flex justify-center pt-60">
+<div class="relative min-h-screen">
+    <div class="flex justify-center absolute w-full top-1/4 ">
         <div class="w-4/12 bg-white p-6 rounded-lg">
 
             @if (session('status'))
-                <div class="bg-red-500 p-4 rounded-lg mb-6 text-white text-center">
-                    {{ session('status') }}
-                </div>
+            <div class="bg-red-500 p-4 rounded-lg mb-6 text-white text-center">
+                {{ session('status') }}
+            </div>
             @endif
 
             <form action="{{ route('login') }}" method="POST">
@@ -16,23 +17,27 @@
 
                 <div class="mb-4">
                     <label for="email" class="sr-only">Email</label>
-                    <input type="email" name="email" id="email" placeholder="Email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror" value="{{ old('email') }}">
+                    <input type="email" name="email" id="email" placeholder="Email"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror"
+                        value="{{ old('email') }}">
 
                     @error('email')
-                        <div class="text-red-500 mt-2 text-sm">
-                            {{ $message }}
-                        </div>
+                    <div class="text-red-500 mt-2 text-sm">
+                        {{ $message }}
+                    </div>
                     @enderror
                 </div>
 
                 <div class="mb-4">
                     <label for="password" class="sr-only">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Mot de passe" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password') border-red-500 @enderror" value="">
+                    <input type="password" name="password" id="password" placeholder="Mot de passe"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password') border-red-500 @enderror"
+                        value="">
 
                     @error('password')
-                        <div class="text-red-500 mt-2 text-sm">
-                            {{ $message }}
-                        </div>
+                    <div class="text-red-500 mt-2 text-sm">
+                        {{ $message }}
+                    </div>
                     @enderror
                 </div>
 
@@ -44,9 +49,11 @@
                 </div>
 
                 <div>
-                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Se connecter</button>
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Se
+                        connecter</button>
                 </div>
             </form>
         </div>
     </div>
+</div>
 @endsection
