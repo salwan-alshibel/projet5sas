b7d926dfe0fab7f0a2a1f7523c9f89f38334d8ba
s-als <32486253+s-als@users.noreply.github.com>
2021-05-26 02:36:45 +0200

    Add method to add a product in Cart class

--- app/Classes/Cart.php
+++ app/Classes/Cart.php
@@ -6,7 +6,7 @@ class Cart
 {
     public $products = null;
     public $totalQty = 0;
-    public $totalPrice =0;
+    public $totalPrice = 0;
 
     //Always check if a Cart already exist and add its values to a "new" Cart:
     public function __construct($oldCart){
@@ -18,19 +18,22 @@ class Cart
     }
 
     //Add a new product:
-    public function add($product, $id) {
-        $storedProduct = ['qty' => 0, 'price' => $product->price, 'product' => $product];
+    public function add($product, $id, $quantity) {
+        //Store product infos in an array:
+        $storedProduct = ['qty' => $quantity, 'price' => $product->price, 'product' => $product];
+        //If the same product already exist in the cart, only add qty to it :
         if ($this->products) {
             if (array_key_exists($id, $this->products)) {
                 $storedProduct = $this->products[$id];
+                $storedProduct['qty'] += $quantity;
             }
         }
-        $storedProduct['qty']++;
-        $storedProduct['price'] = $product->price * $storedProduct['qty'];
+
+        $storedProduct['totalprice'] = $product->price * $storedProduct['qty'];
 
         $this->products[$id] = $storedProduct;
-        $this->totalQty++;
-        $this->totalPrice += $product->price;
+        $this->totalQty = $storedProduct['qty'];
+        $this->totalPrice = $storedProduct['totalprice'];
     }
 
 }
--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Classes\Cart;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class CartController extends Controller
 {
@@ -21,12 +23,17 @@ class CartController extends Controller
         $product = Product::find($request->id);
         $quantity = $request->quantity;
 
-        //Ajouter le produit à la table cart:
+        //Ajouter le produit à une session Laravel:
+            //->Vérification de l'existence d'un panier en cours, si oui on le récupère:
+        $oldCart = Session::has(('cart')) ? Session::get('cart') : null;
+            //->Instanciation d'un objet Cart:
+        $cart = new Cart($oldCart);
+        $cart->add($product, $product->id, $quantity);
 
+        $request->session()->put('cart', $cart);
+        dd($request->session()->get('cart'));
 
         //dd($_POST, $product, $request->quantity);
         return back()->with('message', 'Produit ajouté');
-        //return Redirect::back()->with('message','Operation Successful !');
-        //return view('checkout.cart');
     }
 }
